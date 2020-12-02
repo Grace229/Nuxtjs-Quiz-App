cd8d9f94f6528d07f810b0965ef51503b6e61c44
Grace229 <itiunggrace@gmail.com>
2020-12-02 14:24:52 +0100

    initial commit

--- components/Question.vue
+++ components/Question.vue
@@ -1,138 +1,57 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-      
-      </div>
-      <v-container v-if="loading">
-    <div class="text-xs-center">
-      <v-progress-circular
-        indeterminate
-        :size="150"
-        :width="8"
-        color="green">
-      </v-progress-circular>
-    </div>
-  </v-container>
-  <v-container v-else>
-     <v-card v-for="(data, i) in datas" :key="data.id">
-        <v-card-title class="headline">
-          {{data.question}}
-        </v-card-title>
-        <v-card-text>
-          <v-card 
-    class="mx-auto"
-    max-width="500"
-  >
-    <v-list shaped>
-      <v-list-item-group
-      >
-        <template>
-        
+        <v-card>
+          <v-card-title class="headline">
+            {{ question.question }}
+          </v-card-title>
+          <v-card-text>
+            <v-card class="mx-auto" max-width="500">
+              <v-list shaped>
+                <v-list-item-group>
+                  <template>
+                    <v-list-item
+                      :value="question.question"
+                      active-class="success"
+                    >
+                      <template v-slot:default="{ active }">
+                        <v-list-item-content  v-for="opt in question.options" :key="opt">
+                          <v-list-item-title>
+                            {{ opt }}
+                          </v-list-item-title>
 
-          <v-list-item
-           
-            :key="`data-${i}`"
-            :value="data"
-            active-class="success"
-          >
-            <template v-slot:default="{ active }">
-              <v-list-item-content>
-                <v-list-item-title >
-                  {{data.correct_answer}}
-                </v-list-item-title>
-              </v-list-item-content>
-
-              <v-list-item-action>
-                <v-checkbox
-                  :input-value="active"
-                  color="success"
-                ></v-checkbox>
-              </v-list-item-action>
-              
-              
-            </template>
-            
-          </v-list-item>
-
-        </template>
-      </v-list-item-group>
-    </v-list>
-        <v-list shaped>
-      <v-list-item-group
-      >
-        <template>
-        
-
-          <v-list-item
-           
-            :key="`data-${i}`"
-            :value="data"
-            active-class="success"
-          >
-            <template v-slot:default="{ active }">
-              <v-list-item-content>
-                <v-list-item-title >
-                  <!-- {{data.correct_answer}} -->
-                </v-list-item-title>
-              </v-list-item-content>
-
-              <v-list-item-action>
-                <v-checkbox
-                  :input-value="active"
-                  color="success"
-                ></v-checkbox>
-              </v-list-item-action>
-              
-              
-            </template>
-            
-          </v-list-item>
-
-        </template>
-      </v-list-item-group>
-    </v-list>
-  </v-card>
-        </v-card-text>
-      
-
-      </v-card>
-      </v-container>
+                        <!-- <v-list-item-action> -->
+                          <v-checkbox
+                            :input-value="active"
+                            color="success"
+                          ></v-checkbox>
+                        </v-list-item-content>
+                        <!-- </v-list-item-action> -->
+                      </template>
+                    </v-list-item>
+                  </template>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
+          </v-card-text>
+        </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
-
-
 export default {
-  components: {
-  
+  data: () => ({
+    arrays: [],
+  }),
+  mounted () {
+    console.log('rubbuish', this.question)
   },
-      data: () => ({
-      arrays:[]
-    }),
-
-   props: {
-
-    loading: {
-      default: false
-    },
-  },
-  async asyncData({ $axios }){
-    try {
-      let response = await $axios.$get('https://opentdb.com/api.php?amount=20&category=9&type=multiple');
-        
-      return{
-       datas: response.results,
-    
-      }
-       this.arrays = response.results.incorrect_answers
- console.log(arrays)
-    
-    } catch (err) {
-      console.log(err)
+  props: {
+    question: {
+      type: Object,
+      default: () => {}
     }
-  }
+  },
 }
 </script>
--- pages/Test.vue
+++ pages/Test.vue
@@ -1,7 +1,9 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center"></div>
+      <div class="text-center">
+        <Question :question="questionToDisplay" />
+      </div>
       <v-card>
         <v-card-actions>
           <v-btn color="success" nuxt @click="prevQuestion"> Previous </v-btn>
@@ -14,33 +16,67 @@
 </template>
 
 <script>
-export default {
-  components: {},
-  data: () => ({
-    arrays: [],
-  }),
+import Question from '../components/Question'
 
-  props: {
-    loading: {
-      default: false,
-    },
+export default {
+  components: {
+    Question
   },
-  async asyncData({ $axios }) {
+  data: () => ({
+    questionsFromApiFormatted: [],
+    questionToDisplay: {},
+    indexOfQuestion: 0
+  }),
+  async fetch () {
     try {
-      let response = await $axios.$get(
-        'https://opentdb.com/api.php?amount=20&category=9&type=multiple'
+      let response = await this.$axios.$get(
+        `https://opentdb.com/api.php?amount=20&$category=${9}&type=multiple`
       )
-console.log("response", response)
-      return {
-        datas: response.results,
-      }
-     
+      this.questionsFromApiFormatted = response.results.map((item) => {
+        const arrayOfOptions = [...item.incorrect_answers, item.correct_answer]
+        return {
+          question: item.question,
+          correct_answer: item.correct_answer,
+          options: this.mixOptions(arrayOfOptions),
+        }
+      })
+      this.questionToDisplay = this.questionsFromApiFormatted[0]
+      console.log(this.questionToDisplay)
     } catch (err) {
       console.log(err)
     }
   },
+  computed: {
+    questionNow () {
+      return this.questionToDisplay
+    }
+  },
   methods: {
-    nextQuestion ()
-  }
+    mixOptions(array) {
+      var currentIndex = array.length,
+        temporaryValue,
+        randomIndex
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex -= 1
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex]
+        array[currentIndex] = array[randomIndex]
+        array[randomIndex] = temporaryValue
+      }
+      return array
+    },
+    nextQuestion() {
+      this.questionToDisplay = this.questionsFromApiFormatted[2]
+      console.log(this.questionToDisplay)
+    },
+    prevQuestion() {
+      console.log('next')
+    },
+  },
 }
 </script>
